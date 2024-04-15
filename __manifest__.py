4e53532e8e2f653787f9d238d64c3078b98c5bf1
dnipy <dnipy@protonmail.com>
2024-04-15 12:57:32 +0330

    Your commit message here

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Hospital Management System',
+    "author" : "dnipy",
+    'version': '1.0',
+    'website': 'https://www.hospital.com',
+    "summary":"hospital system for",
+    "data": [
+        'views/menu.xml',
+        'views/patient.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
